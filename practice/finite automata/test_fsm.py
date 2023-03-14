2e4f28e4f39c938357ff1fb68ff07e2e4c0734cc
Viacheslav Sipakov <sipakov.v@rambler.ru>
2023-03-14 20:07:29 +0300

    Fixed inconsistencies in TL_4way_1button test FSM. Added new test FSM - TL_4way_p_and_t (without button, 2 instances of each TL).

--- practice/finite automata/test_fsm.py
+++ practice/finite automata/test_fsm.py
@@ -6,7 +6,7 @@ from fsm import FSM
 def test_FSM_TL_4way_1button():
     alphabet = {'timeout', 'button1'}
     instructions_set = {'set_timeout', 'p1_red', 'p1_green', 'p1_blinking',
-                        't1_red', 't1_red_yellow', 't1_yellow', 't1_green', 't1_blinking'}
+                        't1_red', 't1_yellow_red', 't1_yellow', 't1_green', 't1_blinking'}
     state_set = {'traffic_go', 'traffic_go_ready', 'traffic_go_change', 'traffic_stopping1', 'traffic_stopping2',
                  'p_go', 'p_stopping', 'traffic_ready'}
     initial_state = 'traffic_go'
@@ -33,6 +33,43 @@ def test_FSM_TL_4way_1button():
                               name='fsm_TL_4way_1button')
     print()
     print(fsm_TL_4way_1button)
-    #fsm_TL_4way_1button.visualize()
-    fsm_TL_4way_1button.generate_code_python('fsm_tl_4way_1btn.py')
+    fsm_TL_4way_1button.visualize()
+    fsm_TL_4way_1button.generate_code_python()
+    #assert triple_ones_dfa.parse(entry) == accepts
+
+
+def test_FSM_TL_4way_p_and_t():
+    alphabet = {'timeout'}
+    instructions_set = {'set_timeout',
+                        'p1_red', 'p1_green', 'p1_blinking',
+                        't1_red', 't1_yellow_red', 't1_yellow', 't1_green', 't1_blinking',
+                        'p2_red', 'p2_green', 'p2_blinking',
+                        't2_red', 't2_yellow_red', 't2_yellow', 't2_green', 't2_blinking',
+                        }
+    state_set = {'ns_go', 'ns_stopping', 'ns_stopped',
+                 'ew_go', 'ew_stopping', 'ew_stopped'}
+    initial_state = 'ns_go'
+    initial_instructions = [('set_timeout', 30), 't1_green', 'p1_green', 't2_red', 'p2_red']
+    final_states = set()
+    transition_map = {
+        'ns_go':          {'timeout': ('ns_stopping',   [('set_timeout', 3),  't1_blinking', 'p1_blinking']),},
+        'ns_stopping':    {'timeout': ('ns_stopped',    [('set_timeout', 3),  't1_yellow', 'p1_red', 't2_yellow_red'])},
+        'ns_stopped':     {'timeout': ('ew_go',         [('set_timeout', 20), 't1_red', 't2_green', 'p2_green'])},
+        'ew_go':          {'timeout': ('ew_stopping',   [('set_timeout', 3),  't2_blinking', 'p2_blinking'])},
+        'ew_stopping':    {'timeout': ('ew_stopped',    [('set_timeout', 3),  't1_yellow_red', 't2_yellow', 'p2_red'])},
+        'ew_stopped':     {'timeout': ('ns_go',         [('set_timeout', 30), 't1_green', 't2_red'])},
+    }
+
+    assert (row in state_set for row in transition_map)
+    assert initial_state in state_set
+    assert final_states.issubset(state_set)
+
+    test_FSM_TL_4way_p_and_t = FSM(alphabet=alphabet, instructions_set=instructions_set, state_set=state_set,
+                              initial_state=initial_state, initial_instructions=initial_instructions,
+                              final_states=final_states, transition_map=transition_map,
+                              name='fsm_tl_4way_p_and_t')
+    print()
+    print(test_FSM_TL_4way_p_and_t)
+    test_FSM_TL_4way_p_and_t.visualize()
+    test_FSM_TL_4way_p_and_t.generate_code_python()
     #assert triple_ones_dfa.parse(entry) == accepts
