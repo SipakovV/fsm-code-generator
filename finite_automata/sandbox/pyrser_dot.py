1a05740bb82e76de82105f1fd7962da21aaf2f1f
Viacheslav Sipakov <sipakov.v@rambler.ru>
2023-05-04 17:48:03 +0900

    Added basic semantics: parsing state declarations and adding to the state set.

--- finite_automata/sandbox/pyrser_dot.py
+++ finite_automata/sandbox/pyrser_dot.py
@@ -2,20 +2,25 @@ from pyrser import grammar, meta
 from pyrser.directives import ignore
 
 
+STATES_SET = set()
+
+
 class FSMDOT(grammar.Grammar):
     """Pyrser DOT parser"""
     entry = "dot"
     grammar = """
     dot = [ graph:>_ eof ]
 
-    graph = [ "digraph" ID '{' stmt_list '}' ]
+    graph = [ @ignore("C/C++") "digraph" ID:n #add_graph(_, n) '{' stmt_list:l '}']
     
     stmt_list = [ stmt stmt_list? ]
     
-    stmt = [ ID ]
+    stmt = [ node_stmt ]
+    
+    node_stmt = [ ID:n #add_state(_, n)]
     
     
-    ID = [ letter_ [letter_ | digit]* ]
+    ID = [ @ignore("null") [ letter_ [letter_ | digit]* ] ]
     
     letter_ = [ letter | '_' ]
     
@@ -31,16 +36,29 @@ multiline_comment = [ "/*" ANY* "*/" ]
 """
 
 
-#@meta.hook(FSMDOT)
-def is_bool(self, ast, b):
-    bval = self.value(b)
-    if bval == "true":
-        ast.node = True
-    if bval == "false":
-        ast.node = False
+@meta.hook(FSMDOT)
+def add_state(self, ast, state_name):
+    state = self.value(state_name)
+    ast.node = state
+    if state in STATES_SET:
+        print('Duplicate state declaration')
+        return False
+    else:
+        STATES_SET.add(state)
+    print(ast)
+    return True
+
+
+@meta.hook(FSMDOT)
+def add_graph(self, ast, graph_name):
+    ast.node = self.value(graph_name)
+    print('graph:', ast)
     return True
 
 
 if __name__ == '__main__':
     dot = FSMDOT()
     res = dot.parse_file('sample.dot')
+
+    print(STATES_SET)
+    #print(res.node[0])
